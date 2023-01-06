e207a222d760513f96804a794e2821ce60a32335
Luisana Rodríguez Castellanos <luisana.rcastellanos@gmail.com>
2023-01-06 18:09:31 +0100

    de vuelta

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -4,7 +4,7 @@ public class Ejemplo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Adios Mundo");
+		System.out.println("Adios de vuelta Mundo");
 	}
 
 }
